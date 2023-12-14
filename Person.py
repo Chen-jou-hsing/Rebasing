aee4758c4f8deda56f46699483e90319d35875b4
Chen-jou-hsing <seed006619@gmail.com>
2023-12-14 23:42:58 +0800

    Add age property setter and getter methods

--- Person.py
+++ Person.py
@@ -1,2 +1,9 @@
 class Person:  #定義名為 Person 的類別
     def __init__(self):  #初始化方法，建立類別的實例
+        self._age = None  # 建立私有變數 _age，用於存放年齡資訊
+    
+    def age(self):
+        return self._age  # 回傳 _age 的值
+
+    def age(self, value):
+        self._age = value  # 設定 _age 的值為傳入的 value
